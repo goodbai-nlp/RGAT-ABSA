ad1a19aef63758d263c6414fb8288ac425bce4bf
muyeby <bxf_hit@163.com>
2020-12-18 13:05:18 +0800

    fix datapath bug

--- build_vocab.sh
+++ build_vocab.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # build vocab for different datasets
-python prepare_vocab.py --data_dir dataset/Restaurants --vocab_dir dataset/Restaurants
-python prepare_vocab.py --data_dir dataset/Laptops --vocab_dir dataset/Laptops
-python prepare_vocab.py --data_dir dataset/Tweets --vocab_dir dataset/Tweets
-python prepare_vocab.py --data_dir dataset/MAMS --vocab_dir dataset/MAMS
+setting=Biaffine/glove
+python prepare_vocab.py --data_dir dataset/$setting/Restaurants --vocab_dir dataset/$setting/Restaurants
+python prepare_vocab.py --data_dir dataset/$setting/Laptops --vocab_dir dataset/$setting/Laptops
+python prepare_vocab.py --data_dir dataset/$setting/Tweets --vocab_dir dataset/$setting/Tweets
+python prepare_vocab.py --data_dir dataset/$setting/MAMS --vocab_dir dataset/$setting/MAMS
